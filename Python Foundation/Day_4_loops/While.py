6a9506e04056bc77399ffbc1a4ef58a14584e32b
Kseonux <7a.giuseppeansellonardotelaumb@gmail.com>
2025-10-29 08:56:20 +0700

    learn about break and else

--- Python Foundation/Day_4_loops/While.py
+++ Python Foundation/Day_4_loops/While.py
@@ -5,21 +5,20 @@ a = 8
 while a < 10:
     print (a)
     a += 1
-else:
-    print ("woy maling woy, selesai")
+
+print ("selesai")
 
 #else jalan jika false, selama while kondisi = true maka akan dijalankan dan dicek lagi dan dijalankan lagi. Dalam case ini loop berakhir setelah kondisi 10 dan diatasnya.
 
 #jika pake fungsi biasa dibawah sebetulnya salah jika tanpa (str(a)). Salah karena 8 bukan string, sebetulnya bisa lebih simpel, tinggal pake petik aja 8nya cuma ya biar keren tapi ini boros jadi ga keren.
-"""
-a = 8
-b = (str(a))
 
-while b.isdigit():
-    print (b)
-else:
-    print (str(a))
-"""
+#a = 8
+#b = (str(a))
+#while b.isdigit():
+#    print (b)
+#else:
+#    print (str(a))
+
 #else jalan jika false, selama while kondisi = true maka akan dijalankan dan dicek lagi dan dijalankan lagi. Dalam caseku akan unlimited loop karena nilai akan terus benar.
 
 a = True
