d3a12113133775c28c79ceae4b4c6ab09f9a548e
Kseonux <7a.giuseppeansellonardotelaumb@gmail.com>
2025-10-31 22:05:52 +0700

    yeah

---
+++ Python Foundation/logical_operator_and_short-circuit_evaluations.py
@@ -0,0 +1,45 @@
+
+black = True
+white = False
+balls = False
+
+if white and black or not balls:
+    print ("you're fine")
+else:
+    print("you aren't fine")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
