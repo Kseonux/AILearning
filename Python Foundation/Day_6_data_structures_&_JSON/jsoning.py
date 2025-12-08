967b7e763e8ec228252bf81f78aca6cf9fdc63ba
Kseonux <7a.giuseppeansellonardotelaumb@gmail.com>
2025-12-08 21:29:26 +0700

    yeah json akhirnya kelar

--- Python Foundation/Day_6_data_structures_&_JSON/jsoning.py
+++ Python Foundation/Day_6_data_structures_&_JSON/jsoning.py
@@ -4,18 +4,35 @@
 import json
 
 json_string = '''
-    {
-    "data1" : ["name": "Founder", "age": 18],
-    "data2" : "{"scores": [90, 85, 92]",
-    "data3" : ["profile": {"height": 173, "goal": 185}]
+{
+    "data1": {
+        "name": "Founder", 
+        "age": 18
+    },
+    "data2" : {
+        "scores": [90, 85, 92]
+    },
+    "data3" : {
+        "profile": {
+            "height": 173, 
+            "goal": 185
+        }
     }
+}
 '''
 
 data = json.loads(json_string)
-print(data)
+data['huhu'] = True
 
+jss = json.dumps(data, indent = 4)
+print(jss)
 
-
+# separators=(',', ':')) Tidak ada spasi setelah koma :
+#   Tidak ada spasi setelah koma
+#   Tidak ada spasi setelah titik dua
+#   Lebih pendek
+#   Lebih hemat storage
+#   Lebih cepat dikirim lewat network
 
 
 
